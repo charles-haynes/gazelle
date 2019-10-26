3bb5bab803b6d3f490af79d08ab375884173df3f
Charles Haynes <ceh+gitlab@ceh.bz>
2019-10-26 18:06:11 +0000

    use mockSQLDriver

--- mockSQLDriver/mockSQLDriver.go
+++ mockSQLDriver/mockSQLDriver.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"reflect"
+	"runtime"
 )
 
 type Call struct {
@@ -13,14 +14,18 @@ type Call struct {
 	args interface{}
 }
 
-type Calls []Call
+type CallLog []Call
 
-var calls = Calls{}
+var Calls = CallLog{}
 
-func (c Calls) Add(name string, args interface{}) {
-	calls = append(calls, Call{name, args})
+func (c CallLog) Append(args interface{}) {
+	rpc := make([]uintptr, 1)
+	runtime.Callers(2, rpc)
+	frame, _ := runtime.CallersFrames(rpc).Next()
+	Calls = append(Calls, Call{frame.Function, args})
 }
-func (c Calls) Contains(name string, args interface{}) bool {
+
+func (c CallLog) Contains(name string, args interface{}) bool {
 	for _, v := range c {
 		if v.name != name {
 			continue
@@ -32,80 +37,80 @@ func (c Calls) Contains(name string, args interface{}) bool {
 	return false
 }
 
-func (c Calls) Print() {
+func (c CallLog) Print() {
 	for _, v := range c {
 		fmt.Printf("%s %#v\n", v.name, v.args)
 	}
 }
 
-func (c *Calls) Reset() {
+func (c *CallLog) Reset() {
 	*c = (*c)[:0]
 }
 
 type MockDriver struct{}
 
 func (d *MockDriver) Open(Name string) (driver.Conn, error) {
-	calls.Add("driver.Open", Name)
+	Calls.Append(Name)
 	return MockConn{}, nil
 }
 
 type MockConn struct{}
 
 func (c MockConn) Begin() (driver.Tx, error) {
-	calls.Add("conn.Begin", nil)
+	Calls.Append(nil)
 	return MockTx{}, nil
 }
 func (c MockConn) Close() error {
-	calls.Add("conn.Close", nil)
+	Calls.Append(nil)
 	return nil
 }
 func (c MockConn) Prepare(query string) (driver.Stmt, error) {
-	calls.Add("conn.Prepare", query)
+	Calls.Append(query)
 	return MockStmt{}, nil
 }
 
 type MockTx struct{}
 
 func (t MockTx) Commit() error {
-	calls.Add("tx.Commit", nil)
+	Calls.Append(nil)
 	return nil
 }
 func (t MockTx) Rollback() error {
-	calls.Add("tx.Rollback", nil)
+	Calls.Append(nil)
 	return nil
 }
 
 type MockStmt struct{}
 
 func (s MockStmt) Close() error {
-	calls.Add("stmt.Close", nil)
+	Calls.Append(nil)
 	return nil
 }
 func (s MockStmt) NumInput() int {
-	calls.Add("stmt.NumInput", nil)
+	Calls.Append(nil)
 	return -1
 }
 func (s MockStmt) Exec(args []driver.Value) (driver.Result, error) {
-	calls.Add("stmt.Exec", args)
+	Calls.Append(args)
 	return driver.ResultNoRows, nil
 }
 func (s MockStmt) Query(args []driver.Value) (driver.Rows, error) {
-	calls.Add("stmt.Query", args)
+	Calls.Append(args)
 	return &MockRows{}, nil
 }
 
 type MockRows struct{}
 
 func (r MockRows) Columns() []string {
-	calls.Add("rows.Columns", nil)
+	Calls.Append(nil)
 	return []string{}
 }
 func (r MockRows) Close() error {
-	calls.Add("rows.Close", nil)
+	Calls.Append(nil)
 	return nil
 }
 func (r MockRows) Next(dest []driver.Value) error {
-	calls.Add("rows.Next", dest)
+	Calls.Append(dest)
 	return io.EOF
 }
 
